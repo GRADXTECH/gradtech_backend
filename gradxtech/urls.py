bbb2fc22c3de82bfe13d2fb623f9b9c8f3563377
Leanstix <leanstixx@gmail.com>
2025-03-21 11:13:04 +0100

    Documentation all set

--- gradxtech/urls.py
+++ gradxtech/urls.py
@@ -1,7 +1,6 @@
-
 from django.contrib import admin
 from django.urls import path
-from rest_fframework import permissions
+from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.urls import path
@@ -17,7 +16,7 @@ schema_view = get_schema_view(
         default_version="v1",
         description="API documentation",
         terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="leanstixx@gmail.com")
+        contact=openapi.Contact(email="leanstixx@gmail.com"),
         license=openapi.License(name="BSD License"),
     ),
     public=True,
